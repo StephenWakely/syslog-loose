561bebbf9129e4b73f87551a354e28841f24b5c1
Brian L. Troutwine <brian@troutwine.us>
2021-02-20 12:36:42 -0800

    Measure cycles per byte, introduce more benchmarks
    This commit introduces new benchmarks. The work done in the previous commit
    suggested that the parser was abnormally slow, about 200Mb/s, and with time
    dominated by the `structured_data` parse. This is not wrong but not a complete
    picture. This commit introduces further commits with longer lines, demonstrating
    that the longer the line the better the throughput. This suggests that there is
    a smallish overhead for parsing each line -- presumed to be allocation for the
    `Message` structure but not demonstrated -- and that this cost is paid off
    quickly once the line length increases.
    
    Commit also introduces measurement of these benchmarks by cycle count, rather
    than wall time. This is more stable, though not as stable as
    [iai](https://github.com/bheisler/iai) will be once it's integrated into the
    criterion toolset.
    
    As it stands, short-lines cost about 10 cycles per byte to parse, long-lines
    cost 1 cycle per byte, rounding up some. If we need to improve this in the
    future we ought to explore what the overhead per line is. One avenue for that is
    to adjust the interface to accept a `&mut Message` to populate, rather than
    allowing it to alloc the `Message` itself.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,20 +1,53 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::{Criterion, Throughput};
+use criterion::{BenchmarkId, Criterion, Throughput};
+use criterion_cycles_per_byte::CyclesPerByte;
 use std::convert::TryInto;
+use std::include_str;
 
-fn parse_bench_compare(c: &mut Criterion) {
-    let log = "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource= \"Application\" eventID=\"1011\"] BOMAn application event log entry...";
+struct Parameter<'a> {
+    line: &'a str,
+    name: &'a str,
+}
+
+static PARAMETERS: [Parameter; 4] = [
+    Parameter {
+        line: include_str!("rfc5424/with_structured_data.txt"),
+        name: "with_structured_data",
+    },
+    Parameter {
+        line: include_str!("rfc5424/with_structured_data_long_msg.txt"),
+        name: "with_structured_data_long_message",
+    },
+    Parameter {
+        line: include_str!("rfc5424/without_structured_data_long_msg.txt"),
+        name: "with_structured_data_long_message",
+    },
+    Parameter {
+        line: include_str!("rfc5424/without_structured_data.txt"),
+        name: "without_structured_data",
+    },
+];
+
+fn parse_bench_rfc5424(c: &mut Criterion<CyclesPerByte>) {
     let mut group = c.benchmark_group("RFC5424");
+    for param in &PARAMETERS {
+        let name = param.name;
+        let line = param.line;
+        let bytes = param.line.len().try_into().unwrap();
 
-    group.throughput(Throughput::Bytes(log.len().try_into().unwrap()));
-    group.bench_function("Original", |b| {
-        b.iter(|| syslog_rfc5424::parser::parse_message(log))
-    });
-    group.bench_function("New", |b| b.iter(|| syslog_loose::parse_message(log)));
+        group.throughput(Throughput::Bytes(bytes));
+        group.bench_with_input(BenchmarkId::new(name, bytes), line, |b, line| {
+            b.iter(|| syslog_loose::parse_message(&line))
+        });
+    }
     group.finish();
 }
 
-criterion_group!(benches, parse_bench_compare);
+criterion_group!(
+    name = benches;
+    config = Criterion::default().with_measurement(CyclesPerByte);
+    targets = parse_bench_rfc5424
+);
 criterion_main!(benches);
