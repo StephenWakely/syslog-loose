4c10922d2cbc633e56f716e51637518487c9ccad
github-action-benchmark <github@users.noreply.github.com>
2026-03-27 20:46:34 +0000

    add Benchmark (cargo) benchmark result for 4ed0d87a0ec2455c0679ce213e27f14a8f6b9970

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774636518625,
+  "lastUpdate": 1774644394857,
   "repoUrl": "https://github.com/StephenWakely/syslog-loose",
   "entries": {
     "Benchmark": [
@@ -47,6 +47,78 @@ window.BENCHMARK_DATA = {
             "name": "RFC5424/without_structured_data/110",
             "value": 947,
             "range": "± 23",
+            "unit": "cycles/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fungus.humungus@gmail.com",
+            "name": "Stephen Wakely",
+            "username": "StephenWakely"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ed0d87a0ec2455c0679ce213e27f14a8f6b9970",
+          "message": "Add benches for 3164 (#51)",
+          "timestamp": "2026-03-27T20:44:32Z",
+          "tree_id": "4483a851b3c13c191320b99caab318dac9be1951",
+          "url": "https://github.com/StephenWakely/syslog-loose/commit/4ed0d87a0ec2455c0679ce213e27f14a8f6b9970"
+        },
+        "date": 1774644394049,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "RFC5424/with_structured_data/177",
+            "value": 1485,
+            "range": "± 46",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC5424/with_structured_data_long_message/1893",
+            "value": 943,
+            "range": "± 9",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC5424/with_structured_data_long_message/1960",
+            "value": 1479,
+            "range": "± 9",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC5424/without_structured_data/110",
+            "value": 944,
+            "range": "± 6",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC3164/simple/57",
+            "value": 1437,
+            "range": "± 12",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC3164/long_msg/201",
+            "value": 1491,
+            "range": "± 7",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC3164/with_structured_data/153",
+            "value": 2494,
+            "range": "± 24",
+            "unit": "cycles/iter"
+          },
+          {
+            "name": "RFC3164/rfc3339_timestamp/170",
+            "value": 2484,
+            "range": "± 20",
             "unit": "cycles/iter"
           }
         ]
